02ca7c8561a09363554fe304c2a66d8cd14bef73
francesco-olivero <75547878+francesco-olivero@users.noreply.github.com>
2021-03-16 12:44:55 +0100

    first

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/Word.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/Word.java
@@ -8,9 +8,17 @@ public class Word {
 	@Override
 	public boolean equals(Object obj) {
 		Word in = (Word) obj;
-		if (this.alienWord.equals(in.alienWord) && this.translation.equals(in.translation))
+		if (this.alienWord.equals(in.getAlienWord()) && this.translation.equals(in.getTranslation()))
 			return true;
 		return false;
+	}
+
+	public String getAlienWord() {
+		return alienWord;
+	}
+
+	public String getTranslation() {
+		return translation;
 	}
 	
 	
