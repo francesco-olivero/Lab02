afda6d9f2add1d2f6e72bdf5779e2e5aa216bd96
francesco-olivero <75547878+francesco-olivero@users.noreply.github.com>
2021-03-16 14:06:38 +0100

    entry point changes

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -2,6 +2,8 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.alien.model.AlienDictionary;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,6 +11,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	AlienDictionary dizionario;
 
     @FXML
     private ResourceBundle resources;
